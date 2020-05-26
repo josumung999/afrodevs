cbb5c9c7485fbbf38fa2282aa8410f76c439e111
Joshua <jmunganga039@gmail.com>
2020-05-26 23:35:09 +0200

    API request bases

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const app = express();
 // Se connecter à la base de données
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'))
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
