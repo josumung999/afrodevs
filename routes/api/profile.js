a5b2d550228e6a28bdcdb06202aa52eceb82ac20
Joshua <jmunganga039@gmail.com>
2020-05-29 17:54:50 +0200

    Gettting the loggedin users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route 	GET api/profile
-// @desc 	Test route
-// @access 	Public
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('Profile route'));
+// @route 	GET api/profile/me
+// @desc 	Get current users profile
+// @access 	Private
+
+router.get('/me', auth, async (req, res) => {
+	try {
+		const profile = await Profile
+			.findOne({ user: req.user.id })
+			.populate('user', ['name', 'avatar']);
+
+		if(!profile) {
+			return res.status(400).json({ msg: 'Profile d\'utilisateur introuvable'});
+		}
+
+		res.json(profile);
+	} catch(err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
