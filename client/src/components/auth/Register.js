7b764aa35c8ada8254e071b830cc978ed9d59b7b
Joshua <jmunganga039@gmail.com>
2020-06-15 22:52:13 +0200

    Finished working on Alert Reducers

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+
+const Register = ({setAlert}) => {
 	const [formData, setFormData] = useState({
 		name: '',
 		email: '',
@@ -19,7 +23,7 @@ const Register = () => {
 	const onSubmit = async e => {
 		e.preventDefault();
 		if(password !== password2) {
-			console.log('Les mots de passe sont différents')
+			setAlert('Les mots de passe sont différents', 'danger');
 		} else {
 			console.log('SUCCESS');
 		}
@@ -82,4 +86,8 @@ const Register = () => {
 	)
 }
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+}
+
+export default connect(null, { setAlert })(Register);
---
+++ client/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import alert from './alert';
+
+
+export default combineReducers({
+	alert
+});
