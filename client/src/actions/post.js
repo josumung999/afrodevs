9c329d988a0b72662a07f289668eccbd7cd494e3
Joshua <jmunganga039@gmail.com>
2020-07-17 17:31:41 +0200

    Finished Working on the PostForm

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,8 @@ import {
 	GET_POSTS,
 	POST_ERROR,
 	UPDATE_LIKES,
-	DELETE_POST
+	DELETE_POST,
+	ADD_POST
 } from './types';
 
 // Get posts
@@ -61,7 +62,7 @@ export const removeLike = id => async dispatch => {
 // Delete post
 export const deletePost = id => async dispatch => {
 	try {
-		const res = await axios.delete(`/api/posts/${id}`);
+		await axios.delete(`/api/posts/${id}`);
 
 		dispatch({
 			type: DELETE_POST,
@@ -69,6 +70,31 @@ export const deletePost = id => async dispatch => {
 		})
 
 		dispatch(setAlert('Publication Supprimée', 'success'));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status }
+		})
+	}
+}
+
+// Add post
+export const addPost = FormData => async dispatch => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}
+
+	try {
+		const res = await axios.post('/api/posts', FormData, config);
+
+		dispatch({
+			type: ADD_POST,
+			payload: res.data
+		})
+
+		dispatch(setAlert('Article Publié', 'success'));
 	} catch (err) {
 		dispatch({
 			type: POST_ERROR,
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from './PostItem';
+import PostForm from './PostForm';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -15,7 +16,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
 			<p className="lead">
 				<i className="fas fa-user"></i> Bienvenue dans la communauté
 			</p>
-			{/* Post Forms */}
+			<PostForm />
 
 			<div className="posts">
 				{posts.map(post => (
