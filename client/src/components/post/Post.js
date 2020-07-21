cf92436ea2381dd603158b1f2c42beb66dd16e99
Joshua <jmunganga039@gmail.com>
2020-07-21 18:39:04 +0200

    Finished Working on the Post features

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 import { getPost } from '../../actions/post';
 
 
@@ -17,6 +18,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
 		<Link to='/posts' className='btn'><i className='fas fa-arrow-left'></i> Retour aux Publications</Link>
 		<PostItem post={post} showActions={false} />
 		<CommentForm postId={post._id} />
+		<div className='comments'>
+			{post.comments.map(comment => (
+				<CommentItem key={comment._id} comment={comment} postId={post._id} />
+			))}
+		</div>
 	</Fragment>
 };
 
